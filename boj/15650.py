4af755c0523076fce644bf2ee2d8d6d3bea417df
guiyomj <wastfg6972@naver.com>
2021-11-27 23:43:41 +0900

    add: 211127

---
+++ boj/15650.py
@@ -0,0 +1,7 @@
+from itertools import combinations
+n,m  = map(int, input().split())
+nums = [i for i in range(1, n + 1)]
+for i in combinations(nums, m):
+    for j in range(m):
+        print(i[j], end=' ')
+    print()
